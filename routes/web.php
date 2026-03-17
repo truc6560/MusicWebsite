2d3f356d94cd05499e44ca8569325dc31d560a38
username <email>
2026-03-18 00:26:45 +0700

    71

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get("/qlsach/themtheloai","App\Http\Controllers\BookController@them_dl");
 Route::get("/qlsach/suatheloai","App\Http\Controllers\BookController@sua_dl");
 Route::get("/qlsach/sachkinhdien","App\Http\Controllers\BookController@laysachkinhdien");
 Route::get("/qlsach/xoatheloai","App\Http\Controllers\BookController@xoa_dl");
+
+Route::get("dsgenre","App\Http\Controllers\BookController@ds");
