40389c912a2d3f62f89f91a0b39739d29e755bdc
pj-ngoc <phanthingoc4647@gmail.com>
2026-03-17 21:46:09 +0700

    các bộ phim của Canada

--- app/Http/Controllers/ViDuController.php
+++ app/Http/Controllers/ViDuController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+
 class ViDuController extends Controller
 {
     function test(){
     $name = "HUB";
     return view('test', ["name1"=>$name]);
  }
+
+    function phimCanada(){
+        $phim = DB::table('movie')->where('country_name', 'Canada')->get();
+        return view('canada', compact('phim'));
+    }
 }
