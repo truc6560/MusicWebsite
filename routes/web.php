dfae3f3097d31e9b00058a574d0227757ce4e6bb
truc6560 <baotruc272@gmail.com>
2026-03-28 01:21:40 +0700

    login logout

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\ViDuController;
 
-Route::get("/dien","App\Http\Controllers\ViDuController@hihi");
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get("/truc","App\Http\Controllers\ViDuController@test1");
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get("/phanthingoc","App\Http\Controllers\ViDuController@test2");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get("nguyenbuiminhtu","App\Http\Controllers\ViDuController@btn");
-
-Route::get("/nguyenthibichtram","App\Http\Controllers\ViDuController@btnn");
-Route::get("/xephangtop10","App\Http\Controllers\ViDuController@topmovies");
-Route::get("/phimdai","App\Htpp\Controllers\ViDuController@getLongMovies");
-
-Route::get('/trang1','App\Http\Controllers\ViduLayoutController@trang1');
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-Route::get('sach/chitiet/{id}','App\Http\Controllers\ViduLayoutController@chitiet');
-Route::get('/sach/theloai/{id_loai}', 'App\Http\Controllers\ViduLayoutController@sachTheoTheLoai');
-
+require __DIR__.'/auth.php';
